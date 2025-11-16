210c572cc3dfb97e21042cff52d5f10fa66a46fe
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-16 10:06:14 +0100

    Migrate to new generic AppImage template

--- make-appimage.sh
+++ make-appimage.sh
@@ -1,30 +1,21 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-ARCH="$(uname -m)"
-VERSION="$(cat ~/version)"
-SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-
-# Variables used by quick-sharun
+ARCH=$(uname -m)
+VERSION=$(pacman -Q audio-sharing | awk '{print $2; exit}')
+export ARCH VERSION
+export OUTPATH=./dist
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export OUTNAME=Audio-Sharing-"$VERSION"-anylinux-"$ARCH".AppImage
-export DESKTOP=/usr/share/applications/de.haeckerfelix.AudioSharing.desktop
 export ICON=/usr/share/icons/hicolor/scalable/apps/de.haeckerfelix.AudioSharing.svg
+export DESKTOP=/usr/share/applications/de.haeckerfelix.AudioSharing.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_GSTREAMER=1
 export STARTUPWMCLASS=audio-sharing # For Wayland, this is 'de.haeckerfelix.AudioSharing', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 export LOCALE_FIX=1 # Fixes crash when it fails to switch libc locale in alpine linux
 
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
-wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
-chmod +x ./quick-sharun
-./quick-sharun /usr/bin/audio-sharing
+quick-sharun /usr/bin/audio-sharing
 
-# Make the AppImage with uruntime
-./quick-sharun --make-appimage
-
-# Prepare the AppImage for release
-mkdir -p ./dist
-mv -v ./*.AppImage* ./dist
-mv -v ~/version     ./dist
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
